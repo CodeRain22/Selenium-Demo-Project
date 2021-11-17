7263a23ca0ca59a33ad22c565a2cde8c6c9d8f19
Bhavankumar Patel <bhavan.patel775@gmail.com>
2021-11-16 22:26:48 -0800

    This is my first commit

---
+++ Selenium-Maven-TestNG-Git-Jenkins/src/test/java/SampleTests.java
@@ -0,0 +1,21 @@
+
+import org.testng.annotations.Test;
+
+public class SampleTests {
+    @Test
+    public void test1(){
+        System.out.println("This is first test");
+    }
+    @Test
+    public void test2(){
+        System.out.println("This is second test");
+    }
+    @Test
+    public void test3(){
+        System.out.println("This is third test");
+    }
+    @Test
+    public void test4(){
+        System.out.println("This is fourth test");
+    }
+}
